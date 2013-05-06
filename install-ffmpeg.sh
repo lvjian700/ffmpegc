ccd7490ad5b3785463ad8666bfb7e170a26ee126
lvjian700 <lvjian700@gmail.com>
2013-05-06 22:55:19 +0800

    Downlaod 1.0.1 ffmpeg source code by default

--- install-ffmpeg.sh
+++ install-ffmpeg.sh
@@ -1,12 +1,23 @@
+VERSION="1.0.1"
+
 echo "install gas-* perl script"
 ./install-gas.sh
 
 echo "download ffmpeg"
-git clone git://source.ffmpeg.org/ffmpeg.git ffmpeg
+set -e
+if [ ! -e ffmpeg-${VERSION}.tar.bz2 ]; then
+	echo "Downloading ffmpeg-${VERSION}.tar.bz2"
+    curl -O  http://ffmpeg.org/releases/ffmpeg-${VERSION}.tar.bz2
+else
+	echo "Using ffmpeg-${VERSION}.tar.bz2"
+fi
+
+tar jxvf ffmpeg-${VERSION}.tar.bz2
+
 
 echo "copy install shell script to ffmpeg"
-cp ./compile-*.sh ffmpeg/
-cd ffmpeg
+cp ./compile-*.sh "ffmpeg-${VERSION}"
+cd "ffmpeg-${VERSION}"
 
 ./compile-armv7.sh
 ./compile-i386.sh
