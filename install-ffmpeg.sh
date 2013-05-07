da07cfb9150dac1b48ae7d304bf5f92e464b8a5a
lvjian700 <lvjian700@gmail.com>
2013-05-07 23:36:08 +0800

    add some echo info

--- install-ffmpeg.sh
+++ install-ffmpeg.sh
@@ -19,8 +19,13 @@ echo "copy install shell script to ffmpeg"
 cp ./compile-*.sh "ffmpeg-${VERSION}"
 cd "ffmpeg-${VERSION}"
 
+echo "compile armv7 ..."
 ./compile-armv7.sh
+
+echo "compile i386 for Simulator ..."
 ./compile-i386.sh
+
+echo "packaging universal version ..."
 ./compile-universal.sh
 
 
