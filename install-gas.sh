9479f7a28e2f814ae89d01558ca1d9847e9b6d9c
lvjian700 <lvjian700@gmail.com>
2013-05-18 20:28:28 +0800

    change gas-* repositories from yuvi to mansr

--- install-gas.sh
+++ install-gas.sh
@@ -1,8 +1,8 @@
 echo "install gas-preproccesor.pr"
-git clone git://github.com/yuvi/gas-preprocessor.git
+git clone git://github.com/mansr/gas-preprocessor.git
 
 echo "copy gas-preprocessor.pl to /usr/sbin"
-sudo cp gas-preprocessor/gas-preprocessor.pl /usr/sbin/
+sudo cp -f gas-preprocessor/gas-preprocessor.pl /usr/sbin/
 
 echo "set execute right"
 chmod +x /usr/sbin/gas-preprocessor.pl
